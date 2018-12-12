764a075ab37ed48d32ace40b68630e022146a475
dagmaragluch <44326629+dagmaragluch@users.noreply.github.com>
2018-12-12 23:36:38 +0100

    Add files via upload
    Bardzo wstępna implementacja metod do podświetlania pól

---
+++ BetaSerwer.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+public class BetaSerwer implements MetodySerwera {
+
+    Plansza plansza = new Plansza();
+    ArrayList<ParaWspolrzednych> listaPodswietlanychPol = new ArrayList<>();
+    ArrayList<ParaWspolrzednych> listaPodswietlanychPol2 = new ArrayList<>();
+
+    int x; int y;
+    ParaWspolrzednych paraWspolrzednych = new ParaWspolrzednych(x, y);
+
+
+    @Override
+    public void gdzie_mozna_przesunac(ParaWspolrzednych paraWspolrzednych) {
+
+        int aktualnyX = paraWspolrzednych.getX();
+        int aktualnyY = paraWspolrzednych.getY();
+
+        ParaWspolrzednych aktualnaParaWsp = new ParaWspolrzednych(aktualnyX, aktualnyY);
+//
+//        ArrayList<ParaWspolrzednych> polaDoSprawdzenia = new ArrayList<>();
+//        //ArrayLista pól do sprawdzenia + for each ???
+//
+//        ParaWspolrzednych sprParaWsp = new ParaWspolrzednych(aktualnaParaWsp.getX()-2, aktualnaParaWsp.getY() );
+//
+//        if(plansza.getZawartoscTablicyOdParyWspolrzednych(sprParaWsp) == 0){
+//            listaPodswietlanychPol.add(sprParaWsp);
+//        }
+    }
+
+    @Override
+    public void gdzie_mozna_przesunac2(int x, int y) {  //czy rozdzielić Arraylisty podswitlanych pol na dwie ???
+
+        if(plansza.getZawartoscTablicy(x+1, y+2) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x+1, y+2));
+        }
+        if(plansza.getZawartoscTablicy(x+2, y) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x+2, y));
+        }
+        if(plansza.getZawartoscTablicy(x+1, y-2) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x+1, y-2));
+        }
+        if(plansza.getZawartoscTablicy(x-1, y-2) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x-1, y-2));
+        }
+        if(plansza.getZawartoscTablicy(x-2, y) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x-2, y));
+        }
+        if(plansza.getZawartoscTablicy(x-1, y+2) == 0){
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x-1, y+2));
+        }
+    }
+
+    @Override
+    public void gdzie_mozna_przeskoczyc(int x, int y) {
+
+        if(plansza.getZawartoscTablicy(x+1, y+2) > 0 && plansza.getZawartoscTablicy(x+1, y+2) !=9 ){    //spr czy pole jest zajęte
+            if(plansza.getZawartoscTablicy(x+2, y+4) == 0)    //spr czy nastepne pole jest wolne
+                listaPodswietlanychPol2.add(new ParaWspolrzednych(x+2, y+4));
+        }
+        if(plansza.getZawartoscTablicy(x+2, y) > 0 && plansza.getZawartoscTablicy(x+2, y) !=9 ){
+            if(plansza.getZawartoscTablicy(x+4, y) == 0)
+                listaPodswietlanychPol2.add(new ParaWspolrzednych(x+4, y));
+        }
+        if(plansza.getZawartoscTablicy(x+1, y-2) > 0 && plansza.getZawartoscTablicy(x+1, y-2) !=9 ){
+            if(plansza.getZawartoscTablicy(x+2, y-4) == 0)
+                listaPodswietlanychPol2.add(new ParaWspolrzednych(x+2, y-4));
+        }
+        if(plansza.getZawartoscTablicy(x-1, y-2) > 0 && plansza.getZawartoscTablicy(x-1, y-2) !=9 ){
+            if(plansza.getZawartoscTablicy(x-2, y-4) == 0)
+                listaPodswietlanychPol2.add(new ParaWspolrzednych(x-2, y-4));
+        }
+        if(plansza.getZawartoscTablicy(x-2, y) > 0 && plansza.getZawartoscTablicy(x-2, y) !=9 ){
+            if(plansza.getZawartoscTablicy(x-4, y) == 0)
+            listaPodswietlanychPol2.add(new ParaWspolrzednych(x-4, y));
+        }
+        if(plansza.getZawartoscTablicy(x-1, y+2) > 0 && plansza.getZawartoscTablicy(x-1, y+2) !=9 ){
+            if(plansza.getZawartoscTablicy(x-2, y+4) == 0)
+                listaPodswietlanychPol2.add(new ParaWspolrzednych(x+2, y+4));
+        }
+    }
+
+
+
+}
