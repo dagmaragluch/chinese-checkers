5e7e83338ad6fc6945133e796038ae5d7ad76557
KatNavara <kat.navara@gmail.com>
2018-12-16 13:21:56 +0100

    Dodałam opisy

--- Gra.java
+++ Gra.java
@@ -47,11 +47,13 @@ public class Gra extends PoczatkoweUstawienia implements MetodyDoGry {
 
     @Override
     public void skonczylem() {
-    	for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
+    	
+    	for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {			//pêtla wymazuj¹ca podœwietlone pola na planszy
 			betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 0);
 		}
 		betaSerwer.wyczysc();
-        tura--;
+        
+		tura--;
         ruch=0;
         czyostatni=false;
         
@@ -61,56 +63,59 @@ public class Gra extends PoczatkoweUstawienia implements MetodyDoGry {
     public void wykonaj_ruch(int x, int y) {
 
         if (!czy_wygral()) {
-        	if(!czyostatni) {
+        	if(!czyostatni) {	
+        		
         		if (ruch == 0) {
-        			if(czyja_tura().getKolorGracza() ==  getZawartoscTablicy(x, y)) {
-        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
-        					betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 0);
+        			if(czyja_tura().getKolorGracza() ==  getZawartoscTablicy(x, y)) { //pêtla licz¹ca mo¿liwoœci
+        				betaSerwer.gdzie_mozna_przesunac(x, y); //dwa rodzaje ruchów s¹ dostepne
+        				betaSerwer.gdzie_mozna_przeskoczyc(x, y); 
+        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) { //podœwietlanie pól
+        					betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 8);
         				}
-        				betaSerwer.wyczysc();
-        				betaSerwer.gdzie_mozna_przesunac(x, y);
-        				betaSerwer.gdzie_mozna_przeskoczyc(x, y);
-        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
-        				betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 8);
-        				}
-        		        staryX=x;
+        		        staryX=x; //zapamietujemy wspolrzedne (pocz¹tek pierwszego podruchu)
         		        staryY=y;
         				return;
         			}
         		}
-        		for (ParaWspolrzednych para : betaSerwer.listaPodswietlanychPol2) {
-        			if (x == para.getX() && y == para.getY()) {
+        		for (ParaWspolrzednych para : betaSerwer.listaPodswietlanychPol2) { 
+        			if (x == para.getX() && y == para.getY()) { //sprawdzamy, czy wybrano przeskok
+
         				betaSerwer.plansza.setZawartoscTablicyOdInt(staryX, staryY, 0);
-        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
+        				
+        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) { //wymazanie podœwietleñ
         					betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 0);
         				}
         				betaSerwer.wyczysc();
+        				
         		        betaSerwer.gdzie_mozna_przeskoczyc(x, y);
-        		        for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
+        		        for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) { //nowe podœwietlanie
         					betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 8);
         				}
-        		        staryX=x;
-        		        staryY=y;
+        		        
         				betaSerwer.plansza.setZawartoscTablicyOdInt(x, y, czyja_tura().getKolorGracza());
+        				
+        		        staryX=x; //mo¿emy dalej siê ruszac, wiêc zapisujemy nowe wspolrzedne
+        		        staryY=y;
         				ruch++;
                         return;
         			}
         		}
-        		for (ParaWspolrzednych para : betaSerwer.listaPodswietlanychPol1) {
-        			if (x == para.getX() && y == para.getY()) {
-        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) {
+        		for (ParaWspolrzednych para : betaSerwer.listaPodswietlanychPol1) { 
+        			if (x == para.getX() && y == para.getY()) { //jeœli wybrano pojedynczy ruch
+        				
+        				for (ParaWspolrzednych pw : betaSerwer.listaPodswietlanychPol) { //wymazujemy podswietlenia i nie szukamy dalszych ruchów
         					betaSerwer.plansza.setZawartoscTablicyOdInt(pw.getX(), pw.getY(), 0);
         				}
         				betaSerwer.wyczysc();
+        				
         				betaSerwer.plansza.setZawartoscTablicyOdInt(staryX, staryY, 0);
         				betaSerwer.plansza.setZawartoscTablicyOdInt(x, y, czyja_tura().getKolorGracza());
         				ruch++;
         				czyostatni=true;
         				return;
         			}
-        		} 
-        	} return;
-             
+        		}
+        	}  
         } else {
             skonczylem();
         }
--- Main.java
+++ Main.java
@@ -23,19 +23,21 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		primaryStage.setTitle("Trylma");
+		
+		//Zawartoœc okna startowego
 
 		Label podajlg = new Label("Liczba graczy:");
-		Label podajlb = new Label("Liczba botÃ³w:");
+		Label podajlb = new Label("Liczba botow:");
 		ObservableList<Integer> liczbagr = FXCollections.observableArrayList(2,3,4,6);
 		ObservableList<Integer> liczbabot = FXCollections.observableArrayList(0,1,2,3,4,5);
 		ChoiceBox liczbagraczy=new ChoiceBox<Integer>(liczbagr);
 		ChoiceBox liczbabotow=new ChoiceBox<>(liczbabot);
-		liczbagraczy.getSelectionModel().select(3);
+		liczbagraczy.getSelectionModel().select(0);
 		liczbabotow.getSelectionModel().select(0);
 
 
 		Button dalej = new Button("Przejdz do gry");
-		dalej.setOnAction(e -> {
+		dalej.setOnAction(e -> { //konstrukcja nowej gry i budowanie planszy
 			
 			grac = new Gra((int)liczbagraczy.getValue());
 			for (int i = 0; i < 17; i++) {
@@ -62,6 +64,7 @@ public class Main extends Application {
 		start = new Scene(wybor, 190,190);
 
 
+		//Zawartosc okna trzymajacego plansze
 
 		Button zakonczture = new Button("Zakoncz Ture");
 		zakonczture.setOnAction(e -> {
@@ -76,12 +79,12 @@ public class Main extends Application {
 		trybgry.setBottom(zakonczture);
 		trybgry.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
 
-
-
 		plan.setPadding(new Insets(10,20,10,20));
 		//plan.getTransforms().add(new Rotate(180, Rotate.X_AXIS));
 		gra = new Scene(trybgry, 610, 750);
 
+		
+		//cechy okna przy otwarciu
 		primaryStage.setScene(start);
 		primaryStage.setResizable(false);
 		primaryStage.show();
@@ -93,14 +96,14 @@ public class Main extends Application {
 
 	}
 
-	private class MyEventHandler implements EventHandler<Event>{
+	private class MyEventHandler implements EventHandler<Event>{ //co robi klikniêcie NA POLE
 
 		@Override
 		public void handle(Event evt) {
 
 			Pole temp = (Pole)evt.getSource();
 			grac.wykonaj_ruch(plan.getRowIndex(temp), plan.getColumnIndex(temp));
-			
+
 		}
 	}
 
